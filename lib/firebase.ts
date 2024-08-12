0739a4d2072c7508bcdebb00b454a6d3d742409d
Bohdana K <bohdana.dev@gmail.com>
2024-08-12 14:01:01 +0300

    Project Setup

---
+++ lib/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwBiShUhEigBdy-SO850ovH3BVvB8TQfA",
+  authDomain: "chat2chat-c4cf6.firebaseapp.com",
+  projectId: "chat2chat-c4cf6",
+  storageBucket: "chat2chat-c4cf6.appspot.com",
+  messagingSenderId: "60472477827",
+  appId: "1:60472477827:web:3c20bb2fc6ebb48569f3b6",
+  measurementId: "G-X4N6VQKDN6",
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+export { app, firestore, auth };
